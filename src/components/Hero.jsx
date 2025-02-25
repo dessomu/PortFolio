d2a68cd736df5b81ad2379fc95605a18eb206f11
Somnath Baidya <84554527+dessomu@users.noreply.github.com>
2025-02-25 15:55:08 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,7 @@ function Hero() {
       <div style={{ width: "50%" }}>
         <h2 className="animated-heading">Hello, I’m Somnath</h2>
         <p>
-          A Front-End developer who loves to design appealing and functional web
-          pages that meet user's expectations of having fun interacting with the
-          product and understanding the workflows.
+          A Front-End developer who loves to design appealing and functional web applications that meet users’ expectations of having a wow experience interacting with the product and understanding the workflows. 
         </p>
       </div>
 
